c166f5f6080fd53cb8396eb065fd86451dafd2b1
APF2000 <arthurpfonseca@gmail.com>
2022-02-16 18:05:47 -0300

    :sparkles: Step by step open banking

--- api/consent.py
+++ api/consent.py
@@ -49,3 +49,25 @@ print(data.decode("utf-8"))
 #     "requestDateTime": "2021-05-21T08:30:00Z"
 #   }
 # }
+
+
+# Nome	Tipo	Obrigatório	Descrição
+# data	object	true	
+# » consentId	string	true	O consentId é o identificador único do consentimento e deverá ser um URN - Uniform Resource Name.
+# Um URN, conforme definido na RFC8141 é um Uniform Resource
+# Identifier - URI - que é atribuído sob o URI scheme "urn" e um namespace URN específico, com a intenção de que o URN
+# seja um identificador de recurso persistente e independente da localização.
+# Considerando a string urn:bancoex:C1DD33123 como exemplo para consentId temos:
+# - o namespace(urn)
+# - o identificador associado ao namespace da instituição transnmissora (bancoex)
+# - o identificador específico dentro do namespace (C1DD33123).
+# Informações mais detalhadas sobre a construção de namespaces devem ser consultadas na RFC8141.
+# » creationDateTime	string(date-time)	true	Data e hora em que o recurso foi criado. Uma string com data e hora conforme especificação RFC-3339, sempre com a utilização de timezone UTC(UTC time format).
+# » status	string	true	Estado atual do consentimento cadastrado.
+# » statusUpdateDateTime	string(date-time)	true	Data e hora em que o recurso foi atualizado. Uma string com data e hora conforme especificação RFC-3339, sempre com a utilização de timezone UTC(UTC time format).
+# » permissions	[string]	true	Especifica os tipos de permissões de acesso às APIs no escopo do Open Banking Brasil - Fase 2, de acordo com os blocos de consentimento fornecidos pelo usuário e necessários ao acesso a cada endpoint das APIs.
+# » expirationDateTime	string(date-time)	true	Data e hora de expiração da permissão. De preenchimento obrigatório, reflete a data limite de validade do consentimento. Uma string com data e hora conforme especificação RFC-3339, sempre com a utilização de timezone UTC(UTC time format).
+# » transactionFromDateTime	string(date-time)	false	Data e hora da transação inicial. Se não for preenchido, a transação terá a data aberta e a data será retornada com a primeira transação disponível. Uma string com data e hora conforme especificação RFC-3339, sempre com a utilização de timezone UTC(UTC time format).
+# » transactionToDateTime	string(date-time)	false	Data e hora final da transação. Se não for preenchido, a transação terá a data aberta e a data será retornada com a ultima transação disponível. Uma string com data e hora conforme especificação RFC-3339, sempre com a utilização de timezone UTC(UTC time format).
+# links	Links	false	Referências para outros recusos da API requisitada.
+# meta	Meta	false	Meta informações referente à API requisitada.
